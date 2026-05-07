94b2accdb713d230472df0b6297b891c3b7c5a0a
jeklinmalvin5-cell <jeklinmalvin5@gmail.com>
2026-05-07 14:13:10 +0900

    Update uts.java

---
+++ uts.java
@@ -0,0 +1,34 @@
+public class tipeData { //blok class}
+    // blok main --> psmv + tab 
+    run | Bedung
+    public static void main(string[] args) {
+        // coding 
+         /*universitas dinamika membutuhkan aplikasi sederhana untuk mrncatat biodata mahasiswa?*/
+         
+         
+         
+         NIM --> string
+         NAMA --> string
+         ALAMAT --> string
+         NO..HP --> string
+         JENIS KELAMIN --> string
+         PROGRAM STUDI --> string
+         DOSEN WALI --> string
+         SISA BOP --> int
+
+         int : untuk perhitungan/kalkulasi angka 
+          */
+
+         //1. Deklarasi variabel
+         string NIM = "25410100083"
+string nama ="jeje";
+
+         //2.print nilai variabel --> sout + tab
+         //cara 1": print tanpa teks tambahan
+         system.out.println(NIM);
+
+         //cara 2": print dengan teks tambahan
+         system.out.println("NIM mahasiswa adalah "+NIM+ "dengan nama"+ nama);
+
+         //dengan operator  escape :\n (untuk enter kebaris baru saat diprint
+         system.out.println("NIM : " +nim +"\n nama :"+ nama);
